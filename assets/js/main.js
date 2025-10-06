ee05ee0f8f46c8dcc4ef8a69be40e2a8125ece01
qusai2713 <qusai2713@gmail.com>
2025-10-07 00:41:55 +0400

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  const t=document.createElement('div');t.className='toast';document.body.appendChild(t);
-  window.toast=(m)=>{t.innerHTML='<div style="background:#333;color:#fff;padding:10px 16px;border-radius:8px;">'+(m||'تم')+'</div>';setTimeout(()=>t.innerHTML='',2000);};
-})();
